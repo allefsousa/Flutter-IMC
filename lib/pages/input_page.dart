599f8f840b0df922b4096f2d2d83c3b443660b50
Allef Sousa <allefsousa_1@hotmail.com>
2020-04-13 00:36:00 -0300

    Criando Container Button

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -28,14 +31,29 @@ class _InputPageState extends State<InputPage> {
                 )),
               ],
             )),
-            Expanded(child: ReusableCard(colour: activeCardColor,)),
+            Expanded(
+                child: ReusableCard(
+              colour: activeCardColor,
+            )),
             Expanded(
                 child: Row(
               children: <Widget>[
-                Expanded(child: ReusableCard(colour: activeCardColor,)),
-                Expanded(child: ReusableCard(colour: activeCardColor,)),
+                Expanded(
+                    child: ReusableCard(
+                  colour: activeCardColor,
+                )),
+                Expanded(
+                    child: ReusableCard(
+                  colour: activeCardColor,
+                )),
               ],
             )),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
           ],
         ));
   }
@@ -51,8 +69,7 @@ class ReusableCard extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-          color:colour,
-          borderRadius: BorderRadius.circular(10.0)),
+          color: colour, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
